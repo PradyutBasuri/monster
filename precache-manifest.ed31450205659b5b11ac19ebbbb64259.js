897407dcfe8f4180ca2e586226fef6efd3ed4e8a
Pradyut <pradyut@unifiedinfotech.net>
2020-06-01 12:11:37 +0530

    Updates

---
+++ precache-manifest.ed31450205659b5b11ac19ebbbb64259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a069e19f2f9b00fba0f4c648154bb118",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "996ef06a6732d93648f8",
+    "url": "/monster/static/css/main.6f51bf0b.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monster/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996ef06a6732d93648f8",
+    "url": "/monster/static/js/main.d04a1040.chunk.js"
+  },
+  {
+    "revision": "9a60bd88096b14df0687",
+    "url": "/monster/static/js/runtime-main.8a185aea.js"
+  }
+]);
